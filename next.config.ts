a056b04935a4e0d4914b14da056a5a4fd120788a
kennedymakokha <katchibo2@gmail.com>
2025-06-10 08:11:39 +0300

    change  url

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['app.kersacco.co.ke',"localhost"],
+    domains: ['app.kersacco.co.ke',"localhost","formbuilder.mtandao.app"],
   },
   productionBrowserSourceMaps: false,
   reactStrictMode: true,
