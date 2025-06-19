5fcd39631e23a2ae5a6562614e14c0859821f0f6
kennedymakokha <katchibo2@gmail.com>
2025-06-19 09:41:41 +0300

    adding category to forms

--- src/app/components/SecretKeyCard.tsx
+++ src/app/components/SecretKeyCard.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { generateSecretKey } from '../../../utils/keygen';
-import { useUpdateKeyMutation } from '../../../store/features/authApi';
+// import { useUpdateKeyMutation } from '../../../store/features/authApi';
 
 export default function SecretKeyCard() {
     const [key, setKey] = useState('');
